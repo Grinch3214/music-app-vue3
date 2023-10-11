31f1a1ec21e05375e35c2e7e75f4f4b755077a13
Max <scorpiomax90@gmail.com>
2023-10-11 20:56:08 +0300

    added constans js file

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -33,42 +33,13 @@
 
 <script setup>
 import { ref } from 'vue'
+import { list } from '../constants.js'
 import SongPlayer from './SongPlayer.vue'
 
 const name = ref('Song list')
 
 const isPlayerVisible = ref(false)
 const currentListIndex = ref(0)
-
-const list = [
-	{
-		id: 1,
-		name: 'Сочиняй мечты',
-		albumName: 'Ясно!',
-		artistName: 'Каста',
-		year: 2012,
-		src: 'https://images.genius.com/37d8d643999044c1a7a73edcfeed06a8.800x800x1.jpg',
-		songSrc: 'https://cdn.drivemusic.club/dl/online/ZwxUNXJ5H57di1lakGFwEQ/1682373715/download_music/2012/02/vladi-kasta-sochinjajj-mechty.mp3',
-	},
-	{
-		id: 2,
-		name: 'Чемпион мира',
-		albumName: 'Несусветное',
-		artistName: 'Каста',
-		year: 2015,
-		src: 'https://images.genius.com/37d8d643999044c1a7a73edcfeed06a8.800x800x1.jpg',
-		songSrc: 'https://cdn.drivemusic.club/dl/online/UrJH847mCSn_gvnqi0m3rA/1682373917/download_music/2015/03/vladi-kasta-chempion-mira.mp3',
-	},
-	{
-		id: 3,
-		name: 'Любой дурак',
-		albumName: 'Чернила осминога',
-		artistName: 'Каста',
-		year: 2021,
-		src: 'https://images.genius.com/37d8d643999044c1a7a73edcfeed06a8.800x800x1.jpg',
-		songSrc: 'https://cdn.drivemusic.club/dl/online/6MHi0Hdmj0Xzf13kDTwWDQ/1682374024/download_music/2021/04/kasta-feat.-monetochka-ljubojj-durak.mp3',
-	}
-]
 
 const playSong = (index) => {
 	currentListIndex.value = index
